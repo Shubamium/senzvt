dccc786e55dd257d90a7b5073bce1749c31bac76
Vincent_Liussyaputra <liusvinv@gmail.com>
2026-05-20 11:48:12 +0700

    Back to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,7 @@ import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), react()],
-  adapter: cloudflare(),
 });
