0e18da95c9c0a9b9aff2e03aba3d8e052d152f77
Vincent_Liussyaputra <liusvinv@gmail.com>
2026-05-27 02:14:06 +0700

    Add sitemap seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), react()],
+  integrations: [icon(), react(), sitemap()],
   output: "static",
+  site: "https://senz.to",
 });
